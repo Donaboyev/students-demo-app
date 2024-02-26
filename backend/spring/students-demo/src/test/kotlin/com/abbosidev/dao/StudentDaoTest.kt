dfe980379fb3ef11e118d728814deb80babf87f2
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2024-02-26 21:36:16 +0500

    first test, student adding

---
+++ backend/spring/students-demo/src/test/kotlin/com/abbosidev/dao/StudentDaoTest.kt
@@ -0,0 +1,38 @@
+package com.abbosidev.dao
+
+import com.abbosidev.entity.Student
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentDaoTest {
+
+    @Autowired
+    private lateinit var studentDao: StudentDao
+
+    private val studentId = Int.MAX_VALUE - 1
+    private val studentName = "TEST NAME"
+    private val studentCourse = "TEST COURSE"
+
+    @Test
+    @Order(1)
+    fun isAddingNewStudent() {
+        val studentsCount = studentDao.getAllStudents().size
+        val newStudent = Student(studentId, studentName, studentCourse)
+        studentDao.insertNewStudent(newStudent)
+        val expected = studentsCount + 1 == studentDao.getAllStudents().size
+        assertThat(expected).isTrue()
+
+        val savedStudent = studentDao.getStudentById(studentId)
+        assertThat(savedStudent != null).isTrue()
+
+        val expectedName = savedStudent?.name == studentName
+        assertThat(expectedName).isTrue()
+
+        val expectedCourse = savedStudent?.course == studentCourse
+        assertThat(expectedCourse).isTrue()
+    }
+}
